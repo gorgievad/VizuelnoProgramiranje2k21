aae9554597a82a721a7ac8f42d9c6db87d7d2fb5
Hristina Kamendolieva <hristina.kamendolieva@students.finki.ukim.mk>
2021-06-11 19:06:39 +0200

    Some bug fixes plus definirani preostanatite funkcii

--- ScientificCalculator/ScientificCalculator/Form1.cs
+++ ScientificCalculator/ScientificCalculator/Form1.cs
@@ -16,10 +16,11 @@ namespace ScientificCalculator
         Double result = 0.0;
         bool done = false;
         Double prev = 0;
+        Double saved = 0;
+        String op = "";
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void backspaceSign_Click(object sender, EventArgs e)
@@ -34,162 +35,230 @@ namespace ScientificCalculator
         private void number3_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == "0" || done == true) // za da ne se napisat brojkite vrz defaultnata nula
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "3";
-            prev = 3;
+            //prev = 3;
         }
 
         private void number8_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" || done == true) 
+            if (textBox1.Text == "0" || done == true)
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "8";
-            prev = 8;
+            //prev = 8;
         }
 
         private void number7_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == "0" || done == true) 
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "7";
-            prev = 7;
+            //prev = 7;
         }
 
         private void number6_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" || done == true) 
+            if (textBox1.Text == "0" || done == true) // za da ne se napisat brojkite vrz defaultnata nula
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "6";
-            prev = 6;
+            //prev = 6;
         }
 
         private void number5_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" || done == true) 
+            if (textBox1.Text == "0" || done == true) // za da ne se napisat brojkite vrz defaultnata nula
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "5";
-            prev = 5;
+            //prev = 5;
         }
 
         private void number4_Click(object sender, EventArgs e)
         {
             if (textBox1.Text == "0" || done == true) 
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "4";
-            prev = 4;
+            //prev = 4;
         }
 
         private void number9_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" || done == true) 
+            if (textBox1.Text == "0" || done == true)
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "9";
-            prev = 9;
+            //prev = 9;
         }
 
         private void number2_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" || done == true) 
+            if (textBox1.Text == "0" || done == true)
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "2";
-            prev = 2;
+            //prev = 2;
         }
 
         private void number1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" || done == true) 
+            if (textBox1.Text == "0" || done == true)
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "1";
-            prev = 1;
+            //prev = 1;
         }
 
         private void number0_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "0" || done == true) 
+            if (textBox1.Text == "0" || done == true)
+            {
                 textBox1.Clear();
+                label1.Text = "";
+            }
             done = false;
             textBox1.Text = textBox1.Text + "0";
-            prev = 0;
+            //prev = 0;
         }
 
         private void periodSign_Click(object sender, EventArgs e)
-        { // za da dozvole samo 1 tocka da moze se napise kaj decimalnite
-            if (textBox1.Text.Contains(".") == false)
-                textBox1.Text = textBox1.Text + ".";
+        { // za da dozvole samo 1 zapirka da moze se napise kaj decimalnite
+            if (textBox1.Text.Contains(",") == false)
+                textBox1.Text = textBox1.Text + ",";
+            prev = Double.Parse(textBox1.Text);
             done = false;
         }
         private void basicoperands(String name)
         {
-            label1.Text = label1.Text + textBox1.Text + name;
-            Double saved = Double.Parse(textBox1.Text);
+            if (textBox1.Text != "")
+            {
+                saved = Convert.ToDouble(textBox1.Text);
+            }
             switch (name)
             {
                 case "+":
                     result = saved + prev;
                     done = true;
+                    label1.Text = Convert.ToString(result);
+                    textBox1.Clear();
                     break;
                 case "-":
                     result = prev - saved;
                     done = true;
+                    label1.Text = Convert.ToString(result);
+                    textBox1.Clear();
                     break;
                 case "*":
                     result = saved * prev;
                     done = true;
+                    label1.Text = Convert.ToString(result);
+                    textBox1.Clear();
                     break; 
                 case "/":
                     if (saved != 0)
                     {
                         result = prev / saved;
                         done = true;
+                        label1.Text = Convert.ToString(result);
+                        textBox1.Clear();
+                        break;
                     }
                     else
                     {
                         MessageBox.Show("Не може да се дели со 0.");
+                        done = true;
+                        label1.Text = Convert.ToString(result);
+                        textBox1.Clear();
+                        break;
                     }
+                case "^":
+                    result = Math.Pow(prev,saved);
+                    done = true;
+                    label1.Text = Convert.ToString(result);
+                    textBox1.Clear();
                     break;
             }
-        } 
+        }
         private void otheroperands(String name)
         {
             label1.Text = label1.Text + name + "(" + textBox1.Text + ")"; // voa e za log (x), ln (x), factorial (x), trigonometriskite
-            //textBox1.Clear();
         }
         private void multSign_Click(object sender, EventArgs e)
         {
-            basicoperands(multSign.Text);
+            prev = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+            op = "*";
+            label1.Text = label1.Text + Convert.ToString(prev) + op;
         }
 
         private void divisionSign_Click(object sender, EventArgs e)
         {
-            basicoperands(divisionSign.Text);
+            prev = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+            op = "/";
+            label1.Text = label1.Text + Convert.ToString(prev) + op;
         }
 
         private void minusSign_Click(object sender, EventArgs e)
         {
-            basicoperands(minusSign.Text);
+            prev = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+            op = "-";
+            label1.Text = label1.Text + Convert.ToString(prev) + op;
         }
 
         private void plusSign_Click(object sender, EventArgs e)
         {
-            basicoperands(plusSign.Text);
-
+            prev = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+            op = "+";
+            label1.Text = label1.Text + Convert.ToString(prev) + op;
         }
 
         private void rootFunction_Click(object sender, EventArgs e)
         {
-            otheroperands(rootFunction.Name);
-            Double tmp = Double.Parse(textBox1.Text);
-            this.result = Math.Sqrt(tmp);
-            finished(result.ToString());
+            if (textBox1.Text != "")
+            {
+                otheroperands("root");
+                Double tmp = Double.Parse(textBox1.Text);
+                this.result = Math.Sqrt(tmp);
+                finished(result.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Мора да внесете број прво");
+            }
         }
         private void finished(String rez)
         {
@@ -201,15 +270,25 @@ namespace ScientificCalculator
 
         private void xpoweryFunction_Click(object sender, EventArgs e)
         {
-            otheroperands(xpoweryFunction.Name);
+            prev = Double.Parse(textBox1.Text);
+            textBox1.Clear();
+            op = "^";
+            label1.Text = label1.Text + Convert.ToString(prev) + op;
         }
 
         private void tenpowerxFunction_Click(object sender, EventArgs e)
         {
-            otheroperands(tenpowerxFunction.Name);
-            Double tmp = Double.Parse(textBox1.Text);
-            this.result = Math.Pow(10, tmp);
-            finished(result.ToString());
+            if (textBox1.Text != "")
+            {
+                otheroperands(tenpowerxFunction.Name);
+                Double tmp = Double.Parse(textBox1.Text);
+                this.result = Math.Pow(10, tmp);
+                finished(result.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Мора да внесете број прво");
+            }
         }
 
         private void logFunction_Click(object sender, EventArgs e)
@@ -244,16 +323,31 @@ namespace ScientificCalculator
 
         private void xsquaredFunction_Click(object sender, EventArgs e)
         {
-            otheroperands(xsquaredFunction.Name);
-            Double tmp = Double.Parse(textBox1.Text);
-            this.result = tmp * tmp;
-            finished(result.ToString()); // kaj via funkcii ima finished povikano oti na obicnite kalkulatori
-            // kaa ke stisnis odma go dava rezulatot ne mora da stisnis na ednakvoto i zatoa ima na site povikano
+            if (textBox1.Text != "")
+            {
+                otheroperands(xsquaredFunction.Name);
+                Double tmp = Double.Parse(textBox1.Text);
+                this.result = tmp * tmp;
+                finished(result.ToString()); // kaj via funkcii ima finished povikano oti na obicnite kalkulatori
+                                             // kaa ke stisnis odma go dava rezulatot ne mora da stisnis na ednakvoto i zatoa ima na site povikano
+            }
+            else
+            {
+                MessageBox.Show("Мора да внесете бројка прво");
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public double factorial_recursion(double number)
+        {
+            if (number == 1 || number == 0)
+                return 1;
+            else
+                return number * factorial_recursion(number - 1);
         }
 
         private void nfactorialFunction_Click(object sender, EventArgs e)
@@ -262,8 +356,8 @@ namespace ScientificCalculator
             {
                 otheroperands("fact");
                 Double tmp = Double.Parse(textBox1.Text);
-                //this.result = Math. moze i ke napisam funkcija za faktoriel 
-                //finished(result.ToString());
+                this.result = factorial_recursion(tmp);
+                finished(result.ToString());
             }
             else
             {
@@ -276,7 +370,7 @@ namespace ScientificCalculator
             if (textBox1.Text == "0") 
                 textBox1.Clear();
             done = false;
-            textBox1.Text = "3.14159265358979323";
+            textBox1.Text = "3,14159265358979323";
         }
 
         private void eSign_Click(object sender, EventArgs e)
@@ -284,7 +378,7 @@ namespace ScientificCalculator
             if (textBox1.Text == "0") 
                 textBox1.Clear();
             done = false;
-            textBox1.Text = "2.71828182845904523";
+            textBox1.Text = "2,71828182845904523";
         }
 
         private void sinFunction_Click(object sender, EventArgs e)
@@ -338,8 +432,8 @@ namespace ScientificCalculator
             {
                 otheroperands(cotFunction.Text);
                 Double tmp = Double.Parse(textBox1.Text);
-                //this.result = Math.C(tmp); 
-                //finished(result.ToString());
+                this.result = Math.Cos(tmp) / Math.Sin(tmp);        //бидејќи нема cot функција во Math библиотеката
+                finished(result.ToString());
             }
             else 
             {
@@ -349,14 +443,67 @@ namespace ScientificCalculator
 
         private void equalSign_Click(object sender, EventArgs e)
         {
-            finished(result.ToString());
+            if (op != "")
+            {
+                basicoperands(op);
+            }
         }
 
         private void absoluteValueFunction_Click(object sender, EventArgs e)
         {
-            otheroperands(absoluteValueFunction.Name);
-            Double tmp = Double.Parse(textBox1.Text);
-            this.result = Math.Abs(tmp);
+            if (textBox1.Text != "")
+            {
+                otheroperands("absoluteValue");
+                Double tmp = Double.Parse(textBox1.Text);
+                this.result = Math.Abs(tmp);
+                finished(result.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Мора да внесете број прво");
+            }
+        }
+
+        private void upperValueSign_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                otheroperands("ceil");
+                Double tmp = Double.Parse(textBox1.Text);
+                this.result = Math.Ceiling(tmp);
+                finished(result.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Мора да внесете број прво");
+            }
+        }
+
+        private void lowerValueSign_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                otheroperands("floor");
+                Double tmp = Double.Parse(textBox1.Text);
+                this.result = Math.Floor(tmp);
+                finished(result.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Мора да внесете број прво");
+            }
+        }
+
+        private void negateSign_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("-"))
+            {
+                textBox1.Text = textBox1.Text.Remove(0,1);
+            }
+            else
+            {
+                textBox1.Text = "-" + textBox1.Text; 
+            }
         }
     }
 }
